b3f0e724609fb0627a6040582f2844ce73d0d1da
vinothini <vinothiniciet@gmail.com>
2024-10-06 15:54:01 +0400

    Added Netflix project

---
+++ Namaste/NetflixProject/netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-500">
+      Let's build Netflix App
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT-MAJOR-PROJECTS/Ecommerce-App/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      shopCart
+    </>
+  )
+}
+
+export default App
